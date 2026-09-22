329b300ac21dbf2e1b32d553fd6de6a40aecae88
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-09-22 21:39:40 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        duplicate = []
+        for i in nums:
+            if i in duplicate:
+                return True
+            else:
+                duplicate.append(i)
+            
+        return False
+
+        
